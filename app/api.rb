3df5c0c1efb19c5709a537cf7b1169d1d9cfce76
Dan Sehl <dan.sehl@gmail.com>
2020-12-05 13:03:55 -0700

    Add connection to Postgres DB and return first entry of table in app

--- app/api.rb
+++ app/api.rb
@@ -1,9 +1,20 @@
 require 'grape'
+require 'sequel'
+require 'pg'
+
+DB = Sequel.connect(
+  adapter: "postgres",
+  host: "db",
+  database: "conduit",
+  user: "postgres",
+  password: "abc"
+)
 
 module Conduit
   class API < Grape::API
+    format :json
     get :hello do
-      { 'hello': 'world' }
+      DB[:new_table].first
     end
   end
 end
