6d65cf2ffd59494e2f2b48c03c2b4962614bbaed
Dan Sehl <dan.sehl@gmail.com>
2020-12-16 19:27:16 -0700

    Remove after_create method from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,2 @@
-require 'jwt'
-
 class User < Sequel::Model
-  def after_create
-    payload = { data: 'test' }
-    token = JWT.encode payload, nil, 'none'
-    self.token = token
-    super
-  end
 end
