c056588cee414641d8edfc127e7c1359098b17e6
Dan Sehl <dan.sehl@gmail.com>
2020-12-18 12:33:31 -0700

    Add uniqueness constraint to user email

--- db/001_tables.rb
+++ db/001_tables.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   change do
     create_table(:users) do
       primary_key :id
-      String :email, null: false
+      String :email, null: false, unique: true
       String :username, null: false
       String :password, null: false
       String :bio
