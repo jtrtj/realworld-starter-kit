e443c72fac4ff35af846581b3e213119c2e61b98
Dan Sehl <dan.sehl@gmail.com>
2020-12-18 12:33:07 -0700

    Add various verbs to api - user login, get user, update user

--- app/api.rb
+++ app/api.rb
@@ -19,7 +19,7 @@ module Conduit
     format :json
 
     namespace 'users' do
-      desc 'Create new user.'
+      desc 'Create a new user.'
       params do
         requires :user, type: Hash do
           requires :username, type: String
@@ -27,14 +27,66 @@ module Conduit
           requires :password, type: String
         end
       end
-
       post do
         response = {}
         user = User.create(params["user"])
-        token = JsonWebToken.encode(user: user.username)
+        token = JsonWebToken.encode(user_id: user.id)
         response.merge!(user.values)
         response[:token] = token
-        response
+        {'user' => response}
+      end
+
+      desc 'Login a user.'
+      params do
+        requires :user, type: Hash do
+          requires :email, type: String
+          requires :password, type: String
+        end
+      end
+      post '/login' do
+        response = {}
+        user = User.where(
+          email: params['user']['email'],
+          password: params['user']['password']
+          ).first
+        token = JsonWebToken.encode(user_id: user.id)
+        response.merge!(user.values)
+        response[:token] = token
+        {'user' => response}
+      end
+    end
+
+    namespace 'user' do
+      desc 'Get current user.'
+      get do
+        response = {}
+        token = headers['Authorization'].split[1]
+        decoded_token = JsonWebToken.decode(token)
+        user = User.find(decoded_token[0]['user_id']).first
+        response.merge!(user.values)
+        response[:token] = token
+        {'user' => response}
+      end
+
+      desc 'Update a user.'
+      params do
+        requires :user, type: Hash do
+          optional :email, type: String
+          optional :username, type: String
+          optional :image, type: String
+          optional :bio, type: String
+          optional :password, type: String
+        end
+      end
+      put do
+        response = {}
+        token = headers['Authorization'].split[1]
+        decoded_token = JsonWebToken.decode(token)
+        user = User.find(decoded_token[0]['user_id']).first
+        user.update(params['user'])
+        response.merge!(user.values)
+        response[:token] = token
+        {'user' => response}
       end
     end
   end
