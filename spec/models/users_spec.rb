7379429e0ca9a76e86e16af04634190730473e31
Dan Sehl <dan.sehl@gmail.com>
2020-12-29 18:10:36 -0700

    Add context for class methods in model test for users

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,7 +1,7 @@
 require './app/models/user'
 
 describe User do
-  context '.authorize!' do
+  context 'Class Methods' do
     before do
       @user = User.create(
         email: 'coolguy@org.gov',
@@ -10,25 +10,47 @@ describe User do
       )
       @token = JsonWebToken.encode(user_id: @user.id)
     end
+    context '.authorize!' do
 
-    it 'Checks if token is valid and returns the User if valid' do
-      incoming_env = {
-        'HTTP_AUTHORIZATION' => "Token #{@token}"
-      }
+      it 'Checks if token is valid and returns the User if valid' do
+        incoming_env = {
+          'HTTP_AUTHORIZATION' => "Token #{@token}"
+        }
 
-      current_user = described_class.authorize!(incoming_env)
-      expect(current_user['user'][:id]).to eq(@user.id)
+        current_user = described_class.authorize!(incoming_env)
+        expect(current_user[:user][:id]).to eq(@user.id)
+      end
     end
-  end
-  context '.create_new' do
-    it 'Creates a new user and returns its attributes with a new jwt' do
-      params = { "user":
-                 {
-                   "email": 'cool@guy.org',
-                   "password": 'ssssecret',
-                   "username": 'coolDude'
-                 } }
-      actual = User.create_new(params)
+    context '.create_new' do
+      it 'Creates a new user and returns its attributes with a new jwt' do
+        initial_user_count = User.count
+        params = { user:
+                   {
+                     :email => 'cool@guy.org',
+                     :password => 'ssssecret',
+                     :username => 'coolDude'
+                   } }
+        new_user = User.create_new(params)
+
+        expect(new_user[:user][:email]).to eq(params[:user][:email])
+        expect(new_user[:user][:username]).to eq(params[:user][:username])
+        expect(new_user[:user][:token]).to_not be_nil
+        expect(User.count).to eq(initial_user_count + 1)
+      end
+    end
+    context '.login' do
+      it "Logs in a user and returns its attributes with a new jwt" do
+        params = { user:
+                   {
+                     :email => @user.email,
+                     :password => @user.password
+                   } }
+        logged_in_user = User.login(params)
+        expect(logged_in_user[:user][:email]).to eq(@user.email)
+        expect(logged_in_user[:user][:username]).to eq(@user.username)
+        expect(logged_in_user[:user][:token]).to_not be_nil
+        expect(logged_in_user[:user][:id]).to eq(@user.id)
+      end
     end
   end
 end
