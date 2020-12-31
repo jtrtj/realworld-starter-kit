567fe2ec006df1224db0c9ab4bd4d1cf837b1162
Dan Sehl <dan.sehl@gmail.com>
2020-12-31 14:28:43 -0700

    Flesh out get current user test

--- spec/api/user/get_current_user_spec.rb
+++ spec/api/user/get_current_user_spec.rb
@@ -8,19 +8,33 @@ describe Conduit::API do
   context 'GET /user' do
     context 'media-type: application/json' do
       before do
-        user = User.create(
+        @user = User.create(
           email: 'coolguy@org.gov',
           username: 'Guy',
           password: 'secretss'
         )
-        token = JsonWebToken.encode(user_id: user.id)
+        @token = JsonWebToken.encode(user_id: @user.id)
 
         header 'Content-Type', 'application/json'
-        header 'Authorization', "Token #{token}"
+        header 'Authorization', "Token #{@token}"
       end
 
       it 'returns a the current User' do
         get 'api/user'
+        expected = {
+          'user' =>
+            {
+              'id' => @user.id,
+              'email' => @user.email,
+              'username' => @user.username,
+              'password' => @user.password,
+              'bio' => @user.bio,
+              'image' => @user.image,
+              'token' => @token
+            }
+        }
+        actual = JSON.parse(last_response.body)
+        expect(actual).to eq(expected)
       end
     end
   end
