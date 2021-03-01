58e5b5f1edcab0ec60aa1af95713e1d5b5ed579c
John Roemer <jroemer@harryfox.com>
2021-02-28 21:05:25 -0700

    Add keyword args to follow creat_new as well.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,8 +3,8 @@ require './db/database'
 class Follow < Sequel::Model(Database.instance.conn)
   unrestrict_primary_key
 
-  def self.create_new(following_user, user)
-    create(user_id: user.id, follower_id: following_user.id)
+  def self.create_new(follower:, user:)
+    create(user_id: user.id, follower_id: follower.id)
   end
 
   def self.exists?(follower:, user:)
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -17,7 +17,7 @@ describe Follow do
 
     context '.create_new' do
       it 'Creates a follower relationship between two users' do
-        described_class.create_new(@following_user, @user)
+        described_class.create_new(follower: @following_user, user: @user)
         follower = described_class.with_pk!([@user.id, @following_user.id])
         expect(follower.user_id).to eq(@user.id)
         expect(follower.follower_id).to eq(@following_user.id)
@@ -26,7 +26,7 @@ describe Follow do
 
     context '.exists?' do
       it 'Checks whether or not a user is following another user' do
-        described_class.create_new(@following_user, @user)
+        described_class.create_new(follower: @following_user, user: @user)
         following = described_class.exists?(follower: @following_user, user: @user)
         not_following = described_class.exists?(follower: @user, user: @following_user)
         expect(following).to be true
